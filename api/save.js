f7c633782f4f12664d780f84d80322d4ff87263f
hz415 <hz415@users.noreply.github.com>
2026-07-14 10:56:28 +0000

    fix: add CORS headers to API endpoints

--- api/save.js
+++ api/save.js
@@ -1,6 +1,15 @@
 const mysql = require('mysql2/promise');
 
 module.exports = async (req, res) => {
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
